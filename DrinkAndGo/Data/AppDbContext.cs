7076d667f55630be5b8e7da02c50c920de690eca
diwashrestha <diwash712@gmail.com>
2020-08-26 14:58:26 +0545

    Addes data to mysql database

---
+++ DrinkAndGo/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using DrinkAndGo.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace DrinkAndGo.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Drink> Drinks { get; set; }
+    public DbSet<Category> Categories { get; set; }
+        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
+    }
+}
--- DrinkAndGo/Startup.cs
+++ DrinkAndGo/Startup.cs
@@ -1,26 +1,40 @@
+using DrinkAndGo.Data;
 using DrinkAndGo.Data.Interfaces;
 using DrinkAndGo.Data.Mocks;
+using DrinkAndGo.Data.Models;
+using DrinkAndGo.Data.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using System.Configuration;
+using System;
 
 namespace DrinkAndGo
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        private IConfigurationRoot _configurationRoot; //entrypoint for configuration data
+
+        public Startup(IWebHostEnvironment hostingEnvironment)
+        {
+            _configurationRoot = new ConfigurationBuilder().SetBasePath(hostingEnvironment.ContentRootPath)
+               .AddJsonFile("appsettings.json")
+               .Build();
+        }
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IDrinkRepository, MockDrinkRepository>();
-            services.AddTransient<ICategoryRepository, MockCategoryRepository>();
+            //server configuration
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(_configurationRoot.GetConnectionString("DefaultConnection")));
+
+            services.AddTransient<IDrinkRepository, DrinkRepository>();
+            services.AddTransient<ICategoryRepository, CategoryRepository>();
             services.AddMvc(option => option.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
         {
             app.UseDeveloperExceptionPage(); // used to render the exception during the development environment
             app.UseStatusCodePages(); // used to show the error page for tha http status code
@@ -29,6 +43,7 @@ namespace DrinkAndGo
             {
                 routes.MapRoute("default", "{controller=Home}/{action=Index}/{Id?}");
             });  // add default for the mvc
+            DbInitializer.Seed(serviceProvider);
         }
     }
 }
