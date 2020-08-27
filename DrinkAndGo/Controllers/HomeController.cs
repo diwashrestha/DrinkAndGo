bc92ac071a45455abf5b142e3ce8b58432ffa315
diwashrestha <diwash712@gmail.com>
2020-08-27 11:33:53 +0545

    Chnaged css file name

---
+++ DrinkAndGo/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using DrinkAndGo.Data.Interfaces;
+using DrinkAndGo.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DrinkAndGo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IDrinkRepository _drinkRepository;
+        public HomeController(IDrinkRepository drinkRepository)
+        {
+            _drinkRepository = drinkRepository;
+        }
+        public ViewResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                PreferredDrinks = _drinkRepository.PreferredDrinks
+            };
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ ShoppingCartsummary.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DrinkAndGo.Components
+{
+    public class ShoppingCartSummary:ViewComponent
+    {
+        private readonly ShoppingCart _shoppingCart;
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var shoppingCartViewModel = new ShoppingCartViewmodel
+            {
+                ShoppingCartSummary = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+            return View(shoppingCartViewModel);
+        }
+    }
+}
